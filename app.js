4a9ced9a377c1c6256a68ab4e9b9107c40c1a93d
Mahmoud Ahmed <64172597+Mahmoud-D@users.noreply.github.com>
2023-01-11 00:18:05 +0200

    second change

--- app.js
+++ app.js
@@ -1,21 +1,33 @@
-const left = document.querySelector('.left')
-const right = document.querySelector('.right')
-const slider = document.querySelector('.slider')
-const images =document.querySelectorAll('.image')
+const left = document.querySelector(".left");
+const right = document.querySelector(".right");
+const slider = document.querySelector(".slider");
+const images = document.querySelectorAll(".image");
 
-let sliderNumer=1;
-let length=images.length
+let sliderNumer = 1;
+let length = images.length;
 
-const nextImage=()=>{
-    slider.style.transform=`translateX(-${sliderNumer*600}px)`;
-        sliderNumer ++;
-}
-const prevImage=()=>{
-    slider.style.transform=`translateX(0px)`;
-    sliderNumer=1;
-}
-right.addEventListener('click',
-()=>{
-    sliderNumer<length ? nextImage() : prevImage()
+const nextImage = () => {
+  slider.style.transform = `translateX(-${sliderNumer * 600}px)`;
+  sliderNumer++;
+};
+const prevImage = () => {
+  slider.style.transform = `translateX(-${(sliderNumer - 2) * 600}px)`;
+  sliderNumer--;
+};
+const getFirstImage = () => {
+  slider.style.transform = `translateX(0px)`;
+  sliderNumer = 1;
+};
 
+const getLastImage = () => {
+  slider.style.transform = `translateX ( -${(length - 1) * 600}px)`;
+  sliderNumer = length;
+};
+
+right.addEventListener("click", () => {
+  sliderNumer < length ? nextImage() : prevImage();
+});
+
+left.addEventListener("click", () => {
+  sliderNumer > 1 ? prevImage() : getLastImage();
 });
